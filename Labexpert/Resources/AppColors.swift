90408f6a7b603bee815687a1eab29c576b51d71c
Roman Gudkov <rgudkoff@gmail.com>
2024-02-10 15:44:49 +0300

    Add color file

--- Labexpert/Resources/AppColors.swift
+++ Labexpert/Resources/AppColors.swift
@@ -5,4 +5,16 @@
 //  Created by Роман Гудков on 10.02.2024.
 //
 
-import Foundation
+import UIKit
+
+enum AppColors {
+    
+    static let accenOrange = UIColor().hex(0xD35400)
+    static let accentGreen = UIColor().hex(0x5ED2D2)
+    static let backGreen = UIColor().hex(0x00916E)
+    static let gray = UIColor().hex(0xECF0F1)
+    static let black = UIColor().hex(0x000000)
+    static let background = UIColor().hex(0xE5E5E5)
+    static let white = UIColor.white
+    
+}
